6acd41dab2a7d2955ca32be5cca7b5033607a0f2
Patrick Armentrout <patrick.armentrout@gmail.com>
2020-09-06 20:31:37 -0600

    Done but final puzzle

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 $(() => {
+   $('#puzzle2-container').hide();
+   $('#puzzle3-container').hide();
+   $('#final-puzzle').hide();
     hideErrors();
     // If you are reading this, it is cheating.  I'm not mad, just disappointed...
     const doorData = [{
       number: 1,
-      password: 'happy hour',
+      password: 'test1',
       solved: false
     },
     {
@@ -17,20 +20,44 @@ $(() => {
       solved: false
     }];
 
-    function unlock(element) {
-      element.classList.toggle("fa-lock");
-      element.classList.toggle("fa-unlock-alt");
+    // function unlock(element) {
+    //   element.classList.toggle("fa-lock");
+    //   element.classList.toggle("fa-unlock-alt");
 
-      element.classList.toggle("large-locked");
-      element.classList.toggle("large-unlocked");
-    }
+    //   element.classList.toggle("large-locked");
+    //   element.classList.toggle("large-unlocked");
+    // }
 
-    for (let door of doorData) {
-      $(`#door${door.number}-form`).click((event) => {
-        console.log('Clicked');
-        openDoor(door);
-      });
-    }
+    // for (let door of doorData) {
+    //   $(`#door${door.number}-form`).click((event) => {
+    //     console.log('Clicked');
+    //     openDoor(door);
+    //   });
+    // }
+
+    $('#door1-form').click(e => {
+      const solved = openDoor(doorData[0]);
+      if (solved) {
+        $('#puzzle1-container').hide();
+        $('#puzzle2-container').show();
+      }
+    });
+
+    $('#door2-form').click(e => {
+      const solved = openDoor(doorData[1]);
+      if (solved) {
+        $('#puzzle2-container').hide();
+        $('#puzzle3-container').show();
+      }
+    });
+
+    $('#door3-form').click(e => {
+      const solved = openDoor(doorData[1]);
+      if (solved) {
+        $('#puzzle3-container').hide();
+        $('#final-puzzle').show();
+      }
+    });
 
     function openDoor(door) {
       console.log('open door');
@@ -38,25 +65,12 @@ $(() => {
 
       if (password === door.password) {
         door.solved = true;
-        const finalSolve = checkAll();
-        if (finalSolve) {
-          $('#myModal').modal();
-        }
-        unlock(document.querySelector(`#lock${door.number}`));  
         hideError(door.number);
       } else {
         door.solved = false;
         showError(door.number);
       }
-    }
-
-    function checkAll() {
-      for (let door of doorData) {
-        if (!door.solved) {
-          return false;
-        }
-      }
-      return true;
+      return door.solved;
     }
 
     function showError(door) {
